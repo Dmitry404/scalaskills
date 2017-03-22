f183c22fe5300eb7861f18ed9d5cb0c8c775af36
Dmitriy Butakov <dbutakov@lohika.com>
2017-03-22 16:03:15 +0200

    2. Create a Companion object for Person class which calculates Person's age

--- phase1/src/main/scala/Person.scala
+++ phase1/src/main/scala/Person.scala
@@ -1,4 +1,15 @@
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class Person (name: String, val dob: String) {
   def getName: String = name
+}
+
+object Person {
+  def calculateAge(person: Person): Int = {
+    val pattern = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+    val dobDate = LocalDate.parse(person.dob, pattern)
+
+    LocalDate.now.getYear - dobDate.getYear
+  }
 }
--- phase1/src/test/scala/PersonSpec.scala
+++ phase1/src/test/scala/PersonSpec.scala
@@ -2,9 +2,15 @@ import org.scalatest._
 
 class PersonSpec extends FlatSpec with Matchers  {
   "A Person class" should "exist" in {
-    val person = new Person("John Doe", "10/10/80")
+    val person = new Person("John Doe", "10/10/1980")
 
     person.getName should be ("John Doe")
-    person.dob should be ("10/10/80")
+    person.dob should be ("10/10/1980")
+  }
+
+  "A Person class" should "have a companion which calculates its age" in {
+    val person = new Person("John Doe", "10/10/1980")
+
+    Person.calculateAge(person) should be (37) // assuming it's 2017 now
   }
 }
