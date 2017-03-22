afbfdc80e6c8c71c4a5b975c2845100bc273e24a
Dmitriy Butakov <dbutakov@lohika.com>
2017-03-22 17:15:21 +0200

    changed DOB to be LocalDate

--- phase1/src/main/scala/Person.scala
+++ phase1/src/main/scala/Person.scala
@@ -1,15 +1,12 @@
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
-class Person (name: String, val dob: String) {
+class Person (name: String, val dob: LocalDate) {
   def getName: String = name
 }
 
 object Person {
-  def calculateAge(person: Person): Int = {
-    val pattern = DateTimeFormatter.ofPattern("dd/MM/yyyy")
-    val dobDate = LocalDate.parse(person.dob, pattern)
-
-    LocalDate.now.getYear - dobDate.getYear
+  def calculateAge(person: Person): Long = {
+    ChronoUnit.YEARS.between(person.dob, LocalDate.now) + 1
   }
 }
--- phase1/src/test/scala/PersonSpec.scala
+++ phase1/src/test/scala/PersonSpec.scala
@@ -1,15 +1,17 @@
+import java.time.{LocalDate, Month}
+
 import org.scalatest._
 
 class PersonSpec extends FlatSpec with Matchers  {
   "A Person class" should "exist" in {
-    val person = new Person("John Doe", "10/10/1980")
+    val person = new Person("John Doe", LocalDate.of(1980, Month.JUNE, 10))
 
     person.getName should be ("John Doe")
-    person.dob should be ("10/10/1980")
+    person.dob should be (LocalDate.of(1980, Month.JUNE, 10))
   }
 
   "A Person class" should "have a companion which calculates its age" in {
-    val person = new Person("John Doe", "10/10/1980")
+    val person = new Person("John Doe", LocalDate.of(1980, Month.OCTOBER, 10))
 
     Person.calculateAge(person) should be (37) // assuming it's 2017 now
   }
