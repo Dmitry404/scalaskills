ba36f22566cfb36dc8026769af269ef7cc78d5bf
Dmitriy Butakov <dbutakov@lohika.com>
2017-03-24 00:52:45 +0200

    Updated readme and added a main method for task 6

--- phase1/src/main/scala/Primes.scala
+++ phase1/src/main/scala/Primes.scala
@@ -4,4 +4,8 @@ object Primes {
       num > 1 && !2.to(num/2).toList.exists(num % _ == 0)
     })
   }
+
+  def main(args: Array[String]): Unit = {
+    println(s"Given input contains prime numbers: ${contains(args.map(n => n.toInt).toList)}")
+  }
 }
